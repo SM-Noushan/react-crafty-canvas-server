96ab0f9081c204281de37d21edd64f35aa4c2b58
SMNoushan <noushan.md@gmail.com>
2024-04-27 12:19:59 +0600

    initialization

---
+++ index.js
@@ -0,0 +1,52 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 6969;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// database link
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.b6wqjn1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    // console.log(
+    //   "Close your deployment. You successfully terminate connection to MongoDB!"
+    // );
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("CRAFTY CANVAS SERVER IS RUNNING");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
