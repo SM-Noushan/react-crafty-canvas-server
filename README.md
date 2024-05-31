67c6afbef903ba44e438fcad915e2fdbff4c5fdd
SMNoushan <noushan.md@gmail.com>
2024-05-31 15:17:20 +0600

    vercel deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+# Crafty Canvas - SERVER SIDE
+
+CLIENT: [Crafty Canvas](https://github.com/programming-hero-web-course-4/b9a10-server-side-SM-Noushan)
--- index.js
+++ index.js
@@ -25,12 +25,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     // Connect to the "crafty-canvas" database and access its "painting-and-drawing" collection
     const craftyCanvasDB = client.db("crafty-canvas");
